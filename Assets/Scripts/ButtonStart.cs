c2d5b683cf29287676a9f32e15cb00bc85238a98
Augusto <augus611@gmail.com>
2023-11-30 20:37:22 -0300

    Agregamos movimiento a los topos. Por ahora solo al tocar el botón pero bueno es un avance.

--- Assets/Scripts/ButtonStart.cs
+++ Assets/Scripts/ButtonStart.cs
@@ -9,10 +9,12 @@ public class ButtonStart : MonoBehaviour
 {
     bool gameStart;
     float counter;
-    public DecrementUIText decrementUIText;
+    public Text counterText;
+    public topos toposScript;
     // Start is called before the first frame update
     void Start()
     {
+        toposScript = GameObject.FindGameObjectWithTag("mesa").GetComponent<topos>();
         gameStart = false;
         counter = 5;
     }
@@ -22,10 +24,10 @@ public class ButtonStart : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        counterText.text = ((int)counter).ToString();
         if (gameStart)
         {
             counter -= Time.deltaTime;
-            Debug.Log(counter);
             if (counter < 0)
             {
                 gameStart = false;
@@ -36,51 +38,7 @@ public class ButtonStart : MonoBehaviour
 
     public void Empezar()
     {
-        Debug.Log("empezamos?????????");
+        toposScript.moverTopoRandom();
         gameStart = true;
-        decrementUIText.DecrementText();
     }
 }
-
-
-    /// <summary>
-    /// Add this component to a GameObject and call the <see cref="DecrementText"/> method
-    /// in response to a Unity Event to update a text display to count up with each event.
-    /// </summary>
-    public class DecrementUIText : MonoBehaviour
-    {
-        [SerializeField]
-        [Tooltip("The Text component this behavior uses to display the Decremented value.")]
-        Text m_Text;
-
-        /// <summary>
-        /// The Text component this behavior uses to display the Decremented value.
-        /// </summary>
-        public Text text
-        {
-            get => m_Text;
-            set => m_Text = value;
-        }
-
-        int m_Count;
-
-        /// <summary>
-        /// See <see cref="MonoBehaviour"/>.
-        /// </summary>
-        protected void Awake()
-        {
-            if (m_Text == null)
-                Debug.LogWarning("Missing required Text component reference. Use the Inspector window to assign which Text component to Decrement.", this);
-        }
-
-        /// <summary>
-        /// Decrement the string message of the Text component.
-        /// </summary>
-        public void DecrementText()
-        {
-            m_Count -= 1;
-            if (m_Text != null)
-                m_Text.text = m_Count.ToString();
-        }
-    }
-
---
+++ Assets/Scripts/topos.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class topos : MonoBehaviour
+{
+    public Rigidbody[] arrayTopos;
+    public float movespeed = 1;
+    public float maxHeight = 1;
+    public float minHeight = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void moverTopoArriba(int indexTopo)
+    {
+        if (arrayTopos[indexTopo].transform.position.y < maxHeight)
+        {
+            arrayTopos[indexTopo].transform.position += (Vector3.up * movespeed) * Time.deltaTime;
+        }
+    }
+
+    public void moverTopoRandom()
+    {
+        int randomNumber = Random.Range(0,3);
+        moverTopoArriba(randomNumber);
+    }
+}
